b54c627cff6faa95358c80fd474767d22971ba8a
Haranycheva Mariia <mimimashkabumazka@gmail.com>
2024-08-23 00:03:21 +0300

    chatlist

---
+++ src/components/ChatInformation/ChatInformation.jsx
@@ -0,0 +1,20 @@
+import './ChatInformation.css'
+import UserIcon from "../UserIcon/UserIcon";
+import UserAvatar from "../../assets/user.png";
+
+function ChatInformation({chat}) {
+  return (
+    <li className='chat-inf__wrapp'>
+      <UserIcon imgPath={UserAvatar} isOnline={true}/>
+      <div className='chat-inf__details'>
+        <div>
+            <h3 className='chat-inf__name'>Alice Freeman</h3>
+            <p className='chat-inf__last-message'>How was your meeting?</p>
+        </div>
+        <p className='chat-inf__date'>Aug, 17, 2022</p>
+      </div>
+    </li>
+  )
+}
+
+export default ChatInformation
--- src/components/ChatList/ChatList.jsx
+++ src/components/ChatList/ChatList.jsx
@@ -1,9 +1,15 @@
+import ChatInformation from "../ChatInformation/ChatInformation"
 import "./ChatList.css"
 
 function ChatList() {
   return (
     <div>
-      
+      <h2 className="chats__title">Chats</h2>
+      <ul>
+          <ChatInformation/>
+          <ChatInformation/>
+          <ChatInformation/>
+      </ul>
     </div>
   )
 }
