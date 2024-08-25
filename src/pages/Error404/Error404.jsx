2d9c4688b27da1c135b060d37a779c0ee68671fc
Haranycheva Mariia <mimimashkabumazka@gmail.com>
2024-08-25 15:59:06 +0300

    .

--- src/pages/Error404/Error404.jsx
+++ src/pages/Error404/Error404.jsx
@@ -1,13 +1,21 @@
-import "./Error404.css"
+import "./Error404.css";
 import Cat from "../../assets/gray-cat.jpg";
+import { useEffect, useState } from "react";
+import { Navigate } from "react-router-dom";
 
 function Error404() {
-  return (
+  const [shouldThePageChange, setShouldThePageChange] = useState(false);
+  useEffect(() => {
+    setTimeout(() => setShouldThePageChange(true), 10000);
+  });
+  return shouldThePageChange ? (
+    <Navigate to="/mychats" />
+  ) : (
     <div className="error__wrapp">
       <h1 className="error__title"> 404 ERROR</h1>
-      <img src={Cat} alt="cutie" width="500px" height="500px"/>
+      <img src={Cat} alt="cutie" width="500px" height="500px" />
     </div>
-  )
+  );
 }
 
-export default Error404
+export default Error404;
