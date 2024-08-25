60df6f10193351f660ed7b1e3ea920be50a2200d
Haranycheva Mariia <mimimashkabumazka@gmail.com>
2024-08-25 14:50:08 +0300

    new cat

--- src/pages/Error404/Error404.jsx
+++ src/pages/Error404/Error404.jsx
@@ -1,11 +1,11 @@
 import "./Error404.css"
-import Cat from "../../assets/gray-cat.png";
+import Cat from "../../assets/gray-cat.jpg";
 
 function Error404() {
   return (
     <div className="error__wrapp">
       <h1 className="error__title"> 404 ERROR</h1>
-      <img src={Cat} alt="cutie" />
+      <img src={Cat} alt="cutie" width="500px" height="500px"/>
     </div>
   )
 }
