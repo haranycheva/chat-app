dde2b5ebe3d77577fb39f3c54c7aeb2f9667cc2e
Haranycheva Mariia <mimimashkabumazka@gmail.com>
2024-08-29 21:53:21 +0300

    .

--- src/pages/Messenger/Messenger.jsx
+++ src/pages/Messenger/Messenger.jsx
@@ -3,10 +3,11 @@ import { Outlet } from "react-router-dom"
 import TopMenu from "../../components/TopMenu/TopMenu"
 import ChatList from "../../components/ChatList/ChatList"
 import { useAuth, UserButton, useUser } from "@clerk/clerk-react"
-import { useDispatch } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { useEffect } from "react"
 import { signIn } from "../../fetch"
-
+import { selectLoading } from "../../redux/selectors"
+import Loader from "../../components/Loader.jsx/Loader"
 function Messenger({setModalName, setModalValues}) {
   const { getToken } = useAuth();
   const { user, isLoaded } = useUser();
@@ -27,13 +28,15 @@ function Messenger({setModalName, setModalValues}) {
     fetchUser();
   });
   return (
-    <div className="messenger">
-        <div className="user-nav-wrapp">
-          <TopMenu setModalName={setModalName} userName={user.firstName}/>
-          <ChatList setModalValues={setModalValues} setModalName={setModalName}/>
-        </div>
-        <Outlet/>
-    </div>
+    <>
+      <div className="messenger">
+          <div className="user-nav-wrapp">
+            <TopMenu setModalName={setModalName} userName={user.firstName}/>
+            <ChatList setModalValues={setModalValues} setModalName={setModalName}/>
+          </div>
+          <Outlet/>
+      </div>
+    </>
   )
 }
 
